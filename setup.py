f1134356915ae9a3e661f78fda91e724f4204bea
Hitoshi Harada <hitoshi@alpaca.markets>
2018-10-30 11:20:59 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('alpaca_trade_api/__init__.py', 'rb') as f:
+with open('alpaca_backtrader_api/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
