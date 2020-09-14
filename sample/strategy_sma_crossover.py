fc1bf3f6fd3c9ad32fcfddb38d9b7df9b8f365aa
Shlomi Kushchi <shlomik@gmail.com>
2020-09-14 17:03:02 +0300

    only do logic in next() if we have live bars
    if the data is not live (and we are not backtesting) don't run the algo logic.

--- sample/strategy_sma_crossover.py
+++ sample/strategy_sma_crossover.py
@@ -61,11 +61,16 @@ class SmaCross1(bt.Strategy):
         print('==================================================')
 
     def __init__(self):
+        self.live_bars = False
         sma1 = bt.ind.SMA(self.data0, period=self.p.pfast)
         sma2 = bt.ind.SMA(self.data0, period=self.p.pslow)
         self.crossover0 = bt.ind.CrossOver(sma1, sma2)
 
     def next(self):
+        if not self.live_bars and not IS_BACKTEST:
+            # only run code if we have live bars (today's bars).
+            # ignore if we are backtesting
+            return
         # if fast crosses slow to the upside
         if not self.positionsbyname[symbol].size and self.crossover0 > 0:
             self.buy(data=data0, size=5)  # enter long
