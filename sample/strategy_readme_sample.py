9e14e87dfbc9708a431afa55327d2cbfbd27ff95
Shlomi Kushchi <shlomik@gmail.com>
2021-01-17 19:08:16 +0200

    ci fix

--- sample/strategy_readme_sample.py
+++ sample/strategy_readme_sample.py
@@ -21,10 +21,10 @@ USE_POLYGON = False
 
 
 class SmaCross(bt.SignalStrategy):
-  def __init__(self):
-    sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
-    crossover = bt.ind.CrossOver(sma1, sma2)
-    self.signal_add(bt.SIGNAL_LONG, crossover)
+    def __init__(self):
+        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
+        crossover = bt.ind.CrossOver(sma1, sma2)
+        self.signal_add(bt.SIGNAL_LONG, crossover)
 
 
 if __name__ == '__main__':
